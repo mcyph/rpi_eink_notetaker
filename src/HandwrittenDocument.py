96faf4c78373ae8472c829a313b84cc98721de31
mcyph <20507948+mcyph@users.noreply.github.com>
2021-02-09 21:48:08 +1100

    added basic header text

--- src/HandwrittenDocument.py
+++ src/HandwrittenDocument.py
@@ -116,7 +116,7 @@ class HandwrittenDocument:
                 data_out = BytesIO()
                 _canvas = canvas.Canvas(data_out, pagesize=A4)
                 _canvas.setFont('Helvetica', 24)
-                _canvas.drawCentredString(IMG_WIDTH, IMG_HEIGHT*2, self.name)
+                _canvas.drawCentredString(IMG_WIDTH, A4_SIZE[1], self.name)
 
                 _canvas.drawImage(image, 0,             A4_SIZE[1]//2, IMG_WIDTH, IMG_HEIGHT)
             elif page_num % 4 == 1:

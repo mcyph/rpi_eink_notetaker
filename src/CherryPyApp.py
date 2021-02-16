6a7b42baae2b309f8d943ee01b617d5ab81d0053
mcyph <20507948+mcyph@users.noreply.github.com>
2021-02-16 13:10:21 +1100

    adjusted scale factor

--- src/CherryPyApp.py
+++ src/CherryPyApp.py
@@ -11,7 +11,7 @@ from FramebufferStrokeDisplay import FramebufferStrokeDisplay
 env = Environment(loader=FileSystemLoader(searchpath='./template'),
                   autoescape=True)
 Y_OFFSET_TOP = -20
-Y_SCALE_FACTOR = 0.95
+Y_SCALE_FACTOR = 0.90
 
 
 class App(object):
